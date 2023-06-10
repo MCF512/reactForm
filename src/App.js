47322af7cc20c02e5d6a93c65084b9539ad5bd87
MCF512 <egdem4enko@yandex.ru>
2023-06-10 18:49:26 +0300

    no library form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordRepeat, setPasswordRepeat] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const submitRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!emailError && !passwordError) {
+      console.log({
+        email: email,
+        password: password
+      })
+    }
+  }
+
+  const onEmailBlur = ({ target }) => {
+    let newEmailError = null;
+
+    if (!/(?:@mail.ru)$/.test(target.value)) {
+      newEmailError = 'Почта должна оканчиваться на "@mail.ru"'
+    } else if (target.value.length > 22) {
+      newEmailError = 'Почта не должна быть длинее 22 символов'
+    }
+
+    setEmailError(newEmailError);
+  }
+
+  const onPasswordRepeat = ({ target }) => {
+    let updetedPasswordRepeat = target.value
+    setPasswordRepeat(updetedPasswordRepeat);
+
+    let newPasswordError = null;
+
+    if (password !== updetedPasswordRepeat) {
+      newPasswordError = 'Пароли не совпадают'
+    }
+
+    setPasswordError(newPasswordError)
+  }
+
+  const onFormChange = ({ target }) => {
+    console.log(target.placeholder)
+    if (email && password && passwordRepeat && !passwordError && !emailError) {
+      submitRef.current.focus()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {emailError && <div>{emailError}</div>}
+      {passwordError && <div>{passwordError}</div>}
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        onChange={onFormChange}>
+        <input
+          type='email'
+          value={email}
+          placeholder='email'
+          onChange={({ target }) => setEmail(target.value)}
+          onBlur={onEmailBlur}
+        />
+        <input
+          type='password'
+          value={password}
+          placeholder='пароль'
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <input
+          type='password'
+          value={passwordRepeat}
+          placeholder='повторите пароль'
+          onChange={onPasswordRepeat}
+        />
+
+        <button
+          ref={submitRef}
+          disabled={!email || !password || !passwordRepeat || emailError || passwordError}
+          type='submit'>
+          Зарегистрироваться
+        </button>
+      </form>
     </div>
   );
 }
