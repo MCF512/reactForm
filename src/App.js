0d938842315ca5e8438920cddfd00f343aee2804
MCF512 <egdem4enko@yandex.ru>
2023-06-15 22:00:10 +0300

    useForm and yup

--- src/App.js
+++ src/App.js
@@ -1,90 +1,51 @@
-import { useState, useRef } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import './App.css';
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Почта должна быть валидным адресом электронной почты')
+    .required('Почта обязательна для заполнения')
+    .matches(/(?:@mail.ru)$/, 'Почта должна оканчиваться на "@mail.ru"')
+    .max(22, 'Почта не должна быть длинее 22 символов'),
+  password: yup.string().required('Пароль обязателен для заполнения'),
+  passwordRepeat: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Пароли не совпадают')
+    .required('Подтверждение пароля обязательно для заполнения'),
+});
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordRepeat, setPasswordRepeat] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-  const submitRef = useRef(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!emailError && !passwordError) {
-      console.log({
-        email: email,
-        password: password
-      })
-    }
-  }
-
-  const onEmailBlur = ({ target }) => {
-    let newEmailError = null;
-
-    if (!/(?:@mail.ru)$/.test(target.value)) {
-      newEmailError = 'Почта должна оканчиваться на "@mail.ru"'
-    } else if (target.value.length > 22) {
-      newEmailError = 'Почта не должна быть длинее 22 символов'
-    }
-
-    setEmailError(newEmailError);
-  }
-
-  const onPasswordRepeat = ({ target }) => {
-    let updetedPasswordRepeat = target.value
-    setPasswordRepeat(updetedPasswordRepeat);
-
-    let newPasswordError = null;
-
-    if (password !== updetedPasswordRepeat) {
-      newPasswordError = 'Пароли не совпадают'
-    }
-
-    setPasswordError(newPasswordError)
-  }
-
-  const onFormChange = ({ target }) => {
-    console.log(target.placeholder)
-    if (email && password && passwordRepeat && !passwordError && !emailError) {
-      submitRef.current.focus()
-    }
-  }
+  const password = watch('password');
+  const passwordRepeat = watch('passwordRepeat');
 
   return (
     <div className="App">
-      {emailError && <div>{emailError}</div>}
-      {passwordError && <div>{passwordError}</div>}
-      <form
-        onSubmit={(e) => handleSubmit(e)}
-        onChange={onFormChange}>
-        <input
-          type='email'
-          value={email}
-          placeholder='email'
-          onChange={({ target }) => setEmail(target.value)}
-          onBlur={onEmailBlur}
-        />
-        <input
-          type='password'
-          value={password}
-          placeholder='пароль'
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <input
-          type='password'
-          value={passwordRepeat}
-          placeholder='повторите пароль'
-          onChange={onPasswordRepeat}
-        />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>{errors.email && <div>{errors.email.message}</div>}</div>
+        <div>{errors.password && <div>{errors.password.message}</div>}</div>
+        <div>{errors.passwordRepeat && <div>{errors.passwordRepeat.message}</div>}</div>
+        <input type="email" {...register('email')} placeholder="email" />
+        <input type="password" {...register('password')} placeholder="пароль" />
+        <input type="password" {...register('passwordRepeat')} placeholder="повторите пароль" />
 
-        <button
-          ref={submitRef}
-          disabled={!email || !password || !passwordRepeat || emailError || passwordError}
-          type='submit'>
+        <button type="submit" disabled={!password || !passwordRepeat}>
           Зарегистрироваться
         </button>
       </form>
